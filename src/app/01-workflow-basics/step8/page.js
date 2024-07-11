e455f77d75bf196012e4473d3377b4527a87e422
Filipe Soares <filipe.afns@gmail.com>
2024-07-11 10:41:12 -0300

    Updates route

--- src/app/01-workflow-basics/step8/page.js
+++ src/app/01-workflow-basics/step8/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <Navigation />
       <div className="shot-container">
         <div className="hotspot">
-          <Tooltip title="Hide block from users" text="This toggle allows you to hide a block from your end users. Commonly used when the destination is a data warehouse (in which case you wouldn't want to prompt the user to authenticate)." top={15} left={25} backLink="/01-workflow-builder/step7" nextLink="/01-workflow-basics/step9" direction="left" />
+          <Tooltip title="Hide block from users" text="This toggle allows you to hide a block from your end users. Commonly used when the destination is a data warehouse (in which case you wouldn't want to prompt the user to authenticate)." top={15} left={25} backLink="/01-workflow-basics/step7" nextLink="/01-workflow-basics/step9" direction="left" />
           <img src="/workflow-basics/wf-basics-8.png" />
         </div>
       </div>
--- src/app/01-workflow-basics/step9/page.js
+++ src/app/01-workflow-basics/step9/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <Navigation />
       <div className="shot-container">
         <div className="hotspot">
-          <Tooltip title="Help and Navigation" text="These buttons allow you to zoom in and out of a workflow. Commonly used for large, complex workflows that require a bird's eye view. To the right is the help center where you can find links to our docs and support center." top={69} left={80} backLink="/01-workflow-builder/step8" direction="bottom" />
+          <Tooltip title="Help and Navigation" text="These buttons allow you to zoom in and out of a workflow. Commonly used for large, complex workflows that require a bird's eye view. To the right is the help center where you can find links to our docs and support center." top={69} left={80} backLink="/01-workflow-basics/step8" direction="bottom" />
           <img src="/workflow-basics/wf-basics-1.png" />
         </div>
       </div>
